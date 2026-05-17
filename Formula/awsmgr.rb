181d7b657de2bea2455041b67a7b1b49bc430b57
Santhosh John <santhosh.john@whitbread.com>
2026-05-17 16:49:22 +0100

    Update formula for v1.0.1 release with correct checksums

--- Formula/awsmgr.rb
+++ Formula/awsmgr.rb
@@ -2,22 +2,23 @@ class Awsmgr < Formula
   desc "Operational workflow and environment diagnostics CLI for AWS engineers"
   homepage "https://github.com/santhosh-john/awsmgr"
   license "MIT"
+  version "1.0.1"
   
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/santhosh-john/awsmgr/releases/download/v1.0.0/awsmgr_1.0.0_darwin_arm64.tar.gz"
-      sha256 "2b6b95499100696d469f4780d590ffbfe931d71e62b0fdbe8934fbbe5efff662"
+      url "https://github.com/santhosh-john/awsmgr/releases/download/v1.0.1/awsmgr_1.0.1_darwin_arm64.tar.gz"
+      sha256 "07a9f969c01d0c7a634fc873c43ed2aa06f98f92517cd18b64c40c527568a1c8"
     else
-      url "https://github.com/santhosh-john/awsmgr/releases/download/v1.0.0/awsmgr_1.0.0_darwin_amd64.tar.gz"
-      sha256 "fd240a37a717dc7434cf2b3f9db44e080e6eb17f3282bfc676880df2585f22ca"
+      url "https://github.com/santhosh-john/awsmgr/releases/download/v1.0.1/awsmgr_1.0.1_darwin_amd64.tar.gz"
+      sha256 "824a59f3dc27d76a8e0e7b07de8ea720396e1fdc970456268f24a43fd8c544ed"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/santhosh-john/awsmgr/releases/download/v1.0.0/awsmgr_1.0.0_linux_arm64.tar.gz"
-      sha256 "979db327ba04cf0e12993c2c33dc5292bbabe7845ed02cabb5e37f66308462b8"
+      url "https://github.com/santhosh-john/awsmgr/releases/download/v1.0.1/awsmgr_1.0.1_linux_arm64.tar.gz"
+      sha256 "64c4ec64bddf77aaa719a7e15cc43eb29a124742ae3b06731d5986b0c429fe66"
     else
-      url "https://github.com/santhosh-john/awsmgr/releases/download/v1.0.0/awsmgr_1.0.0_linux_amd64.tar.gz"
-      sha256 "09f78f11fa0bfc03b0b873a4ae0ad7ecca48a1ec8321bfc7d998df34d0df6c2c"
+      url "https://github.com/santhosh-john/awsmgr/releases/download/v1.0.1/awsmgr_1.0.1_linux_amd64.tar.gz"
+      sha256 "51bc0d50e96bf15a9729f2d17fd72a9c08b74de30bf2642145470b1ddb7594b6"
     end
   end
 
